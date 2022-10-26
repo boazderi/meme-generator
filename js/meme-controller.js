320db18a94f617b9b4b3ff9f528e836385b1a0d2
Boaz Deri <boazderi@gmail.com>
2022-10-26 13:16:35 +0300

    first version, basic structure of files with basic html with minimal things

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -2,4 +2,13 @@
 
 function onInit(){
     console.log('start init');
+    renderMeme()
+}
+
+
+function renderMeme(){
+    console.log('start render');
+    getMeme()
+    const elCanvas = document.querySelector('.canvas-container')
+    
 }
